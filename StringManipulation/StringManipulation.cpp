516aabdfbde20ddddb7d3d498cf4ed2ace901d1a
Jose Otavio Quaglio <joseotavio@gmail.com>
2019-10-03 21:56:05 -0300

    Changes on MyMemCpy.

--- StringManipulation/StringManipulation.cpp
+++ StringManipulation/StringManipulation.cpp
@@ -14,29 +14,21 @@ int main()
 void CallMyToLower()
 {
 	std::cout << "[TOLOWER]\n";
-	std::cout << "mytolower('J') = " << mytolower('J') << "\n";
-	std::cout << "mytolower('o') = " << mytolower('o') << "\n";
-	std::cout << "mytolower('S') = " << mytolower('S') << "\n";
-	std::cout << "mytolower('e') = " << mytolower('e') << "\n\n";
+	std::cout << mytolower('J') << mytolower('o') << mytolower('S') << mytolower('e') << "\n\n";
 }
 
 void CallMyToUpper()
 {
 	std::cout << "[TOUPPER]\n";
-	std::cout << "mytoupper('J') = " << mytoupper('J') << "\n";
-	std::cout << "mytoupper('o') = " << mytoupper('o') << "\n";
-	std::cout << "mytoupper('S') = " << mytoupper('S') << "\n";
-	std::cout << "mytoupper('e') = " << mytoupper('e') << "\n\n";
+	std::cout << mytoupper('J') << mytoupper('o') << mytoupper('S') << mytoupper('e') << "\n\n";
 }
 
 void CallMyMemCpy()
 {
-	char src[] = "ABC";
-	char dst[4];
+	char src[] = "First";
+	char dst[] = "Second";
 	mymemcpy(src, dst, 3);
 
-	std::cout << "[TOUPPER]\n";
-	std::cout << "char src[] = \"ABC\";\n";
-	std::cout << "char dst[4];\n";
-	std::cout << "mymemcpy(src, dst, 3); dst = " << dst << "\n";
+	printf("src: %s\n", src);
+	printf("dst: %s\n\n", dst);
 }
--- StringManipulation/crt.cpp
+++ StringManipulation/crt.cpp
@@ -18,13 +18,15 @@ char mytoupper(char ch)
 
 void* mymemcpy(void* src_, void* dst_, unsigned int count)
 {
-	for (int i = 0; i <= count; i++)
+	for (int i = 0; i < count; i++)
 	{
-		char srcChar = ((char*)src_)[i];
-		((char*)dst_)[i] = srcChar;
+		char* srcChar = (char*)src_ + i;
+		char* dstChar = (char*)dst_ + i;
+
+		*dstChar = *srcChar;
 	}
 
-	return nullptr;
+	return dst_;
 }
 
 void* mymemmove(void* src_, void* dst_, unsigned int count)
