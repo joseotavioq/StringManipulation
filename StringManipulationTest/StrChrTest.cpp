0fdaf94a097a419379f2f4918ed07bb904b6859a
Jose Otavio Quaglio <joseotavio@gmail.com>
2019-10-10 20:24:09 -0300

    One more test for the StrChr and StrRChr.

--- StringManipulationTest/StrChrTest.cpp
+++ StringManipulationTest/StrChrTest.cpp
@@ -30,6 +30,16 @@ namespace StringManipulationTest
 			Assert::AreEqual(&sentence[23], result);
 		}
 
+		TEST_METHOD(Search_For_The_Null_Termination_Char_In_The_Middle_Of_A_Sentence)
+		{
+			char sentence[] = "This is a \0simple string";
+			char character = '\0';
+
+			char* result = mystrchr(sentence, character);
+
+			Assert::AreEqual(&sentence[10], result);
+		}
+
 		TEST_METHOD(Not_Found)
 		{
 			char sentence[] = "This is a simple string";
