f3df86f9dee9a475dc766f120af44ffb1377966c
Jose Otavio Quaglio <joseotavio@gmail.com>
2019-11-12 21:34:09 -0300

    Added the HanoiMove unit test class.

---
+++ StringManipulationTest/exercises/HanoiMoveTest.cpp
@@ -0,0 +1,29 @@
+#include "CppUnitTest.h"
+#include "../../StringManipulation/Exercises.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace StringManipulationTest
+{
+	namespace Exercises
+	{
+		TEST_CLASS(HanoiMoveTest)
+		{
+		public:
+
+			/*TEST_METHOD(Move_1_2_3_4_To_Destination_Array)
+			{
+				int* src = new int[4]{ 1, 2, 3, 4 };
+				int* dst = new int[4]{ 0, 0, 0, 0 };
+				int* tmp = new int[4]{ 0, 0, 0, 0 };
+
+				HanoiMove(4, src, dst, tmp);
+
+				Assert::AreEqual(1, dst[0]);
+				Assert::AreEqual(2, dst[1]);
+				Assert::AreEqual(3, dst[2]);
+				Assert::AreEqual(4, dst[3]);
+			}*/
+		};
+	}
+}
